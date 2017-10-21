235e801d25eee564eb582295d30615317bd93b07
user <boakes@github.com>
2017-10-20 23:33:36 -0500

    man moves

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float moveSpeed = 6f;
+	public Rigidbody theRigidBody;
+	// Use this for initialization
+	void Start () {
+		theRigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+		theRigidBody.velocity = new Vector3(inputX*moveSpeed, inputY *moveSpeed,1);
+	}
+}
